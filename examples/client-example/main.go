cc21e4e3bbeda5f9ea08d74754792d01bc0b4ba3
webus <webusnix@gmail.com>
2022-02-15 20:12:52 +0300

    fix demo. format code

--- examples/client-example/main.go
+++ examples/client-example/main.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"os"
 	"time"
-	"github.com/afiskon/promtail-client/promtail"
+
+	"github.com/webus/promtail-client/promtail"
 )
 
 func displayUsage() {
@@ -20,13 +21,13 @@ func displayInvalidName(arg string) {
 
 func nameIsValid(name string) bool {
 	for _, c := range name {
-        if !((c >= 'a' && c <= 'z') ||
-             (c >= 'A' && c <= 'Z') ||
-             (c >= '0' && c <= '9') ||
-             (c == '-') || (c == '_')) {
-            return false
-        }
-    }
+		if !((c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z') ||
+			(c >= '0' && c <= '9') ||
+			(c == '-') || (c == '_')) {
+			return false
+		}
+	}
 	return true
 }
 
@@ -50,19 +51,19 @@ func main() {
 		displayInvalidName("job-name")
 	}
 
-	labels := "{source=\""+source_name+"\",job=\""+job_name+"\"}"
+	labels := "{source=\"" + source_name + "\",job=\"" + job_name + "\"}"
 	conf := promtail.ClientConfig{
 		PushURL:            "http://localhost:3100/api/prom/push",
 		Labels:             labels,
 		BatchWait:          5 * time.Second,
 		BatchEntriesNumber: 10000,
-		SendLevel: 			promtail.INFO,
-		PrintLevel: 		promtail.ERROR,
+		SendLevel:          promtail.INFO,
+		PrintLevel:         promtail.ERROR,
 	}
 
 	var (
 		loki promtail.Client
-		err error
+		err  error
 	)
 
 	if format == "proto" {
